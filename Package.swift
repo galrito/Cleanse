f58a4af463bebceb44d62c42bc7995e590055fa4
Jorge Galrito <jorge.galrito@me.com>
2019-10-12 14:50:06 +0100

    Update SwiftPM manifest file

--- Package.swift
+++ Package.swift
@@ -1,8 +1,13 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "Cleanse",
+    products: [
+        .library(name: "Cleanse", targets: ["Cleanse"])
+    ],
     targets: [
+        .target(name: "Cleanse"),
+        .testTarget(name: "CleanseTests", dependencies: [.target(name: "Cleanse")])
     ]
 )
-
